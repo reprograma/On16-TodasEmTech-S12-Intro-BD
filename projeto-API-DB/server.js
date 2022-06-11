5cb59ab3a515c6642a097df1572280424d166427
MajoBueno <buenomariajoao@gmail.com>
2022-06-11 16:39:21 -0300

    conectamos servidor com mongodb

--- projeto-API-DB/server.js
+++ projeto-API-DB/server.js
@@ -5,3 +5,9 @@ const port = 3000
 app.listen(port, () => {
     console.log(`Servidor está rodando na porta ${port}`)
 });
+
+
+
+
+
+// mongodb+srv://majob:<password>@majob.jaeuzld.mongodb.net/?retryWrites=true&w=majority
