f1d454602dc1b2a22400f44a371b826f0fae08a9
lialaurindo <liamaralaurindo@gmail.com>
2022-06-11 19:06:50 -0300

    importando db para a aplicação

--- projeto-API-DB/src/app.js
+++ projeto-API-DB/src/app.js
@@ -1,11 +1,20 @@
 const express = require ("express");
 const index = require ("./routes/index.js"); 
 const livros = require ("./routes/livros.js");
+const db = require ("./config/dbConnect.js");
+
+db.on('error', console.log.bind (console, 'Erro de conexão'))
+
+db.once('open', () => {
+    console.log ('conexão com o banco feita com sucesso')
+});
+
 
 const app = express();
 
 app.use(express.json());
 
+// transmissão de comunicação http x client
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*") 
     res.header(
