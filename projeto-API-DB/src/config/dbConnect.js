678b81eca2049ad8d982e4eca3cc13312a833782
MajoBueno <buenomariajoao@gmail.com>
2022-06-11 17:41:28 -0300

    dbConnect.js corrigido

--- projeto-API-DB/src/config/dbConnect.js
+++ projeto-API-DB/src/config/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 // outra opção é importar com import
 // import mongoose from "mongoose";
 //  conectando mangoose com o link do mongodb
-mongoose.connect("mongodb+srv://majob:santa1020@majob.jaeuzld.mongodb.net/reprograma")
+mongoose.connect("")
 
 // pra poder exportar a conecção, gravo ela numa variavel e 
 let db = mongoose.connection
