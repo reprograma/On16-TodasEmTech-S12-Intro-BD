9138765610dcc13f41713b3fb337dfc1c67e59af
Bárbara Baron <bcostabaron@gmail.com>
2022-06-13 20:13:33 -0300

    atualiza dbConnect

--- projeto-API-DB/src/config/dbConnect.js
+++ projeto-API-DB/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://new-user25:reprograma123@cluster0.1vuycki.mongodb.net/reprograma");
+mongoose.connect('mongodb+srv://bcostabaron:ortiga18@cluster0.pjltxsr.mongodb.net/reprograma');
 
 let db = mongoose.connection;
 
