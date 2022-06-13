28777a28c521ccf33850b85d153cb61c0c7f6a8e
PriscillaCorreia <primariafc@gmail.com>
2022-06-13 19:15:36 -0300

    muda de json para Schema

---
+++ projeto-API-DB/src/models/livros.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const livroSchema = new mongoose.Schema({
+    id: {type: String}, // id n é requerido
+    titulo:{type: String, required: true},
+    autora:{type: String, required: true},
+    editora:{type: String, required: true},
+    numeroPaginas:{type: Number, required: true},
+});
+
+ 
+  
+const livros= mongoose.models('livros',livroSchema);
+
+module.exports = livros;
