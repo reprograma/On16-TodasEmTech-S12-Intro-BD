f55f21041f32e7627f69530512ca110cb4b0220c
Bárbara Baron <bcostabaron@gmail.com>
2022-06-13 21:16:33 -0300

    adiciona sintaxe de conexão no arquivo app.js

--- games/src/app.js
+++ games/src/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
-
+const db = require ("./config/dbConnect.js")
 const gamesRoutes = require('./router/gamesRoutes')
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+    console.log('Conexão com o banco feita com sucesso')
+});
+
 
 const app = express()
 
