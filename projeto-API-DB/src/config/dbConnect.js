3b7d8f85227d7294f02594c7dee5a10360787d42
Gaia Maria <gaiamaria.melos@gmail.com>
2022-06-15 19:41:03 -0300

    :bulb: docs: Melhora sintaxe do codigo e testa

--- projeto-API-DB/src/config/dbConnect.js
+++ projeto-API-DB/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://GaiaMaria8:thilbo2020@cluster0.btpmh.mongodb.net/reprograma");
+mongoose.connect("mongodb+srv://GaiaMaria8:@cluster0.btpmh.mongodb.net/reprograma");
 
 let db = mongoose.connection;
 
