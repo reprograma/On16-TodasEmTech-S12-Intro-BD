9ac3f8087540ee8b66fc5054e29418c39ac5d6c3
Bárbara Baron <64811382+babixbaron@users.noreply.github.com>
2022-06-16 15:05:42 -0300

    Delete dbConnect.js

--- projeto-API-DB/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require ("mongoose");
-
-mongoose.connect('mongodb+srv://bcostabaron:ortiga18@cluster0.pjltxsr.mongodb.net/reprograma');
-
-let db = mongoose.connection;
-
-module.exports = db;
