d72850fa5b88c532aa97192eb48ebda82ca17592
Bárbara Baron <bcostabaron@gmail.com>
2022-06-16 17:24:18 -0300

    refatorando getGamesById

--- games-API-DB/src/controller/gamesController.js
+++ games-API-DB/src/controller/gamesController.js
@@ -7,3 +7,16 @@ const getAllGames = (req, res) => {
      res.status(200).json(games)
     })
  }
+
+ const getGamesById = (req, res) => {
+
+    const id = req.params.id
+
+    games.findById(id, (err, games) => {
+        if(err) {
+            res.status(404).send({message: `${err.message} - Game ID not found`})
+        } else {
+            res.status(200).send(games)
+        }
+    })
+}
