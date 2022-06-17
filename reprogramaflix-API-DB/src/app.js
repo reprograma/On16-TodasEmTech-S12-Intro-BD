810e182102f9be104b7c9346d606862fe9709e07
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-06-17 14:46:47 -0300

    Centralizando conteúdo da aplicação - rota raiz

---
+++ reprogramaflix-API-DB/src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const index = require("./routes/index.js");
+const series = require("./routes/seriesRoutes.js");
+
+const db = require("./config/dbConnect.js");
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+    console.log('Conexão com o banco feita com sucesso')
+});
+
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+})
+
+// Nossas rotas raiz
+app.use("/", index);
+app.use("/reprogramaflix", series);
+
+
+
+module.exports = app;
