1daf7b455a422f115f01a426887f22653ef62677
Ana Ribeiro <analidialirar@gmail.com>
2022-06-17 16:54:23 -0300

    fix: correct db conection string

--- games-API-DB/src/config/dbConnect.js
+++ games-API-DB/src/config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(`mongodb+srv://${process.env.ATLAS_LOGIN}:${process.env.ATLAS_PASSWORD}@cluster0.hgy1c7l.mongodb.net/week-12`)
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_LOGIN}:${process.env.ATLAS_PASSWORD}@cluster0.c1wu8.mongodb.net/week-12`)
 
 let db = mongoose.connection;
 
