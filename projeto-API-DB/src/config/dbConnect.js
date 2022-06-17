1fb2a7d4eb0a71ea1f82f2ccae3f640993be82a6
Nathercia <dev.nathercia@gmail.com>
2022-06-17 14:58:43 -0300

    Criado tarefa de casa

--- projeto-API-DB/src/config/dbConnect.js
+++ projeto-API-DB/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://new-user25:reprograma123@cluster0.1vuycki.mongodb.net/reprograma");
+mongoose.connect("mongodb+srv://Nathercia:tetinha18@cluster0.3t6tc6u.mongodb.net/reprograma");
 
 let db = mongoose.connection;
 
