172427f3b6057712bd6e54cbf56c49c2d7bca079
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-06-17 14:58:01 -0300

    rotas CRUD

---
+++ reprogramaflix-API-DB/src/routes/seriesRoutes.js
@@ -0,0 +1,17 @@
+// Criando nossas rotas
+
+const controller = require('../controllers/seriesController.js')
+
+const express = require('express')
+
+// Função de rotas do express
+const router = express.Router()
+
+// router, verbo http (caminho, função)
+router.get("/series", controller.getAllSeries)
+router.get("/series/:id", controller.getSerieById)
+router.post("/novaserie", controller.createSeries)
+router.delete("/series/delete/:id", controller.deleteSerieById)
+
+// Exportar para ser usado no app.js
+module.exports = router
