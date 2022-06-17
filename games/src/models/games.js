7c059290ceb9645d21da991b46322610fb8b79e8
Louise <louicosta.silva@gmail.com>
2022-06-17 15:28:46 -0300

    Adiciona schema

---
+++ games/src/models/games.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const gameSchema = new mongoose.Schema({
+  id: { type: String },
+  title: { type: String, required: true },
+  launchYear: { type: Number, required: true },
+  consoles: { type: Array, required: true },
+  liked: { type: Boolean, required: true }
+});
+
+
+const games = mongoose.model('games', gameSchema);
+
+module.exports = games;
