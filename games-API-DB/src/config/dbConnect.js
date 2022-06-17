2997aabf0e590ce31f952c8ee18976f888ee16fc
Ana Ribeiro <analidialirar@gmail.com>
2022-06-17 16:51:19 -0300

    feat: add db conection

---
+++ games-API-DB/src/config/dbConnect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_LOGIN}:${process.env.ATLAS_PASSWORD}@cluster0.hgy1c7l.mongodb.net/week-12`)
+
+let db = mongoose.connection;
+
+module.exports = db 
