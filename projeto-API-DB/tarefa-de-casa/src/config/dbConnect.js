b6bb02fd253b64627981c6f390d3c76b9e681257
heloaaires <heloa.andrade@gmail.com>
2022-06-17 18:59:58 -0300

    retira exposição da senha

--- projeto-API-DB/tarefa-de-casa/src/config/dbConnect.js
+++ projeto-API-DB/tarefa-de-casa/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://heloaaires:Mongo1380@cluster0.taceygj.mongodb.net/?retryWrites=true&w=majority/reprograma");
+mongoose.connect("mongodb+srv://heloaaires:@cluster0.taceygj.mongodb.net/?retryWrites=true&w=majority/reprograma");
 
 let db = mongoose.connection;
 
