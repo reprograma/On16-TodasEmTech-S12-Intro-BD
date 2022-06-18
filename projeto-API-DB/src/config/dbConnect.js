e0570316c7455aa68a2104086ae558b55fac4f06
camisarp <camilarp.rec@gmail.com>
2022-06-18 00:02:26 -0300

    Week 12 Activities

--- projeto-API-DB/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require ("mongoose");
-
-mongoose.connect('mongodb+srv://username:<password>@cluster0.1vuycki.mongodb.net/?retryWrites=true&w=majority');
-
-let db = mongoose.connection;
-
-module.exports = db;
