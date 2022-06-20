906df4ef117ee535cc4220e2b57d2b6236ec082b
Maysafig <maysafig@gmail.com>
2022-06-20 20:21:59 -0300

    Correção da configuração do banco

--- para-casa/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require ("mongoose");
-
-mongoose.connect("mongodb+srv://Maysafig:061121wedding@cluster0.piizrrl.mongodb.net/reprograma");
-
-let db = mongoose.connection;
-
-module.exports = db;
