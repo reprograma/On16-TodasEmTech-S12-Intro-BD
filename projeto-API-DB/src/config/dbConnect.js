b183cc2b9f8ed044f2d8f7e981d01e511d3064ba
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-06-26 16:44:01 -0300

    instalação de dependencias e config db

--- projeto-API-DB/src/config/dbConnect.js
+++ projeto-API-DB/src/config/dbConnect.js
@@ -1,4 +1,4 @@
-const mongoose = require ("mongoose");
+const mongoose = require ("mongoose")
 
 mongoose.connect('mongodb+srv://username:<password>@cluster0.1vuycki.mongodb.net/?retryWrites=true&w=majority');
 
