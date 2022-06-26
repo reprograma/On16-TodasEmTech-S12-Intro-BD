6b68b93d977b4eb4dc2d08c935bc4c540b6e8acc
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-06-26 16:46:26 -0300

    configuração BD

--- projeto-API-DB/src/config/dbConnect.js
+++ projeto-API-DB/src/config/dbConnect.js
@@ -1,6 +1,8 @@
 const mongoose = require ("mongoose")
 
-mongoose.connect('mongodb+srv://username:<password>@cluster0.1vuycki.mongodb.net/?retryWrites=true&w=majority');
+//mongoose.connect('mongodb+srv://username:<password>@cluster0.1vuycki.mongodb.net/?retryWrites=true&w=majority');
+
+mongoose.connect('mongodb+srv://claudianefidelis:<password>@cluster0.ijgvj.mongodb.net/?retryWrites=true&w=majority');
 
 let db = mongoose.connection;
 
