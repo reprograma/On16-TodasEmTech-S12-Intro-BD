bdee69da823118d0cc510170ed43fa3404fb871d
NILVA PIRES DA SILVA <piresnilva@gmail.com>
2022-07-12 13:51:59 -0300

    Update and rename projeto-API-DB/src/config/dbConnect.js to projeto-API-DB/src/configuração/dbConnect.js

--- projeto-API-DB/src/configuração/dbConnect.js
+++ projeto-API-DB/src/configuração/dbConnect.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose"); // importe o mongoose
 //connect para receber o string de conexao do mongoDB Atlas
 mongoose.connect(
-  "mongodb+srv://piresnilva:sofmeL2012@cluster0.7rbouil.mongodb.net/reprograma",
-  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
