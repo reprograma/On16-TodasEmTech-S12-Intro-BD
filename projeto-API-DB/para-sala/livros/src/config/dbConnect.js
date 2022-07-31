5114cca03f2ed2e00a9ae34e4ad06f49ddcffb80
Lia Laurindo <liamaralaurindo@gmail.com>
2022-07-30 23:28:21 -0300

    criaçao de ambiente de teste

--- projeto-API-DB/para-sala/livros/src/config/dbConnect.js
+++ projeto-API-DB/para-sala/livros/src/config/dbConnect.js
@@ -1,26 +1,11 @@
-const mongoose = require ('mongoose');
+const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://user_01:852585@cluster0.pzaygyu.mongodb.net/reprograma");
+mongoose.connect("mongodb://localhost:27017/Reprograma", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
-//exportar nossa conexão
 
 let db = mongoose.connection;
 
 module.exports = db;
-
-
-// //{
-//     "id": 1,
-//     "titulo": "Pequeno Manual Antiracista",
-//     "autor": "Djamila Ribeiro",
-//     "editora": "Letramento",
-//     "numeroPaginas": 120
-// }
-
-// db.getCollection("livros").find({})
-
-// db.getCollection("livros").insertOne({"id":"3", "titulo": "cartas para minha avó", "autor": "Djamila Ribeiro", "numeroPaginas": 150})
-
-//db.getCollection("livros").updateMany({editora: "letramento"}, {$set: {autor: "Antoine de Saint - Exupéry"}})
-
-// db.getCollection("livros").deleteOne({autor: "Djamila Ribeiro"})
